0cc01161610e4bfc18a3594c7f37199e93abb847
nattapong2848 <nattapong2848@gmail.com>
2026-05-23 16:25:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// วาง URL ที่ได้จาก Google Apps Script ตรงนี้ หลัง Deploy
-// ตัวอย่าง: window.API_URL = "https://script.google.com/macros/s/xxxx/exec";
-window.API_URL = "";
+https://script.google.com/macros/s/AKfycbwpgF9GqapBgvDycObcfSUDEI46vrMxBQhqoEFhFLUn-1DzhQBvHicVWlcj-Z69Sb1o9g/exec
