54afe16b5e2890fd683f39b02579dcd776370a14
nattapong2848 <nattapong2848@gmail.com>
2026-05-23 16:35:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -24,11 +24,8 @@ function init(){
 init();
 
 function apiUrl(){
-  return (window.API_URL || "function apiUrl(){
   return "https://script.google.com/macros/s/AKfycbx8o0Fb7XvANoagptA795gdYhvbnsVC4sYNoxsSEP6HpaHFPEKgh1_GMe5AYgyrBZNgsA/exec";
-}").trim();
 }
-
 async function callApi(action, payload={}){
   if(!apiUrl()){
     throw new Error("ยังไม่ได้ใส่ API URL ในไฟล์ config.js");
